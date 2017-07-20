e888b311882c4254e5dd4928e1d5f28f7cad7476
Vinícius Pereira <jedipereira@gmail.com>
2017-07-20 17:49:38 -0300

    refactor to use Puma

--- server.rb
+++ server.rb
@@ -46,78 +46,82 @@ class BookSerializer
 end
 
 # Endpoints
-get '/' do
-	'Welcome to Booklist'
-end
-
-namespace '/api/v1' do
+class MyAppRoutes < Sinatra::Base
+	register Sinatra::Namespace
 	
-	before do
-		content_type 'application/json'
+	get '/' do
+		'Welcome to Booklist'
 	end
 
-	helpers do
-		def base_url
-      @base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
-    end
-
-		def json_params
-      begin
-        JSON.parse(request.body.read)
-      rescue
-        halt 400, { message:'Invalid JSON' }.to_json
-      end
-		end
+	namespace '/api/v1' do
 		
-		def book
-			@book ||= Book.where(id: params[:id]).first
+		before do
+			content_type 'application/json'
 		end
 
-		def halt_if_not_found!
-			halt(404, { message: 'Book Not Found' }.to_json) unless book
+		helpers do
+			def base_url
+				@base_url ||= "#{request.env['rack.url_scheme']}://{request.env['HTTP_HOST']}"
+			end
+
+			def json_params
+				begin
+					JSON.parse(request.body.read)
+				rescue
+					halt 400, { message:'Invalid JSON' }.to_json
+				end
+			end
+			
+			def book
+				@book ||= Book.where(id: params[:id]).first
+			end
+
+			def halt_if_not_found!
+				halt(404, { message: 'Book Not Found' }.to_json) unless book
+			end
+
+			def serialize(book)
+				BookSerializer.new(book).to_json
+			end
 		end
 
-		def serialize(book)
-			BookSerializer.new(book).to_json
-		end
-	end
+		get '/books' do
+			books = Book.all
 
-	get '/books' do
-		books = Book.all
+			[:title, :isbn, :author].each do |filter|
+				books = books.send(filter, params[filter]) if params[filter]
+			end
 
-		[:title, :isbn, :author].each do |filter|
-			books = books.send(filter, params[filter]) if params[filter]
+			books.map { |book| BookSerializer.new(book) }.to_json
 		end
 
-		books.map { |book| BookSerializer.new(book) }.to_json
-	end
+		get '/books/:id' do |id|
+			halt_if_not_found!
 
-	get '/books/:id' do |id|
-		halt_if_not_found!
-		
-		serialize(book)
-	end
+			serialize(book)
+		end
 
-	post '/books' do
-		book = Book.new(json_params)
+		post '/books' do
+			book = Book.new(json_params)
 
-		halt 422, serialize(book) unless book.save
+			halt 422, serialize(book) unless book.save
 
-		response.headers['Location'] = "#{base_url}/api/v1/books/#{book.id}"
-		status 201
-	end
+			response.headers['Location'] = "#{base_url}/api/v1/books/#{book.id}"
+			status 201
+		end
 
-	patch '/books/:id' do |id|
-		halt_if_not_found!
-		halt 422, serialize(book) unless book.update_attributes(json_params)
+		patch '/books/:id' do |id|
+			halt_if_not_found!
+			halt 422, serialize(book) unless book.update_attributes(json_params)
 
-		serialize(book)
-	end
+			serialize(book)
+		end
 
-	delete '/books/:id' do |id|
-		book.destroy if book
-		
-		status 204
-	end
+		delete '/books/:id' do |id|
+			book.destroy if book
+			
+			status 204
+		end
 
-end
+	end # end-namespace
+end # end-class
